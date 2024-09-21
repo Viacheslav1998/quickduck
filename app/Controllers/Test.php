06f5b9c4e6efe43f725e67dd4bc1d82248dcbb5e
Slavik <amazingSevenfix@gmail.com>
2024-09-21 22:04:18 +0500

    first commit

---
+++ app/Controllers/Test.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App\Controllers;
+
+/**
+ * just for the sake of understanding
+ */
+class Test extends BaseController
+{
+	
+	public function test(): string 
+	{
+		return view('pages/test');
+	}
+}
---
+++ app/Views/pages/Test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Just tesing</title>
+</head>
+<body style="background: silver; padding: 0; margin: 0; font-family: Arial; text-align: center;">
+	<h1>hello everyone</h1>
+	<p>simple project</p>
+	<b>you can see Test page</b>
+</body>
+</html>
