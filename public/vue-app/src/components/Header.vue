14de82c1b265cbc1716591746df45375401291a8
Slavik <amazingSevenfix@gmail.com>
2024-09-26 00:24:11 +0500

    quickfix

--- public/vue-app/src/components/Header.vue
+++ public/vue-app/src/components/Header.vue
@@ -1,19 +1,21 @@
 <template>
   <!-- navbar  -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <RouterLink class="navbar-brand" to="/">Домой</RouterLink>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <nav class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <RouterLink class="nav-link" to="/about">О нас </RouterLink>
-        </li>
-      </nav>
-      <span class="navbar-text">найдет тот кто ищет...</span>
-    </div>
-  </nav>
+  <div class="navi-box container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <RouterLink class="navbar-brand" to="/">Домой</RouterLink>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <nav class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/about">О нас </RouterLink>
+          </li>
+        </nav>
+        <span class="navbar-text">найдет тот кто ищет...</span>
+      </div>
+    </nav>
+  </div>
   <!-- end navbar  -->
 </template>
 
