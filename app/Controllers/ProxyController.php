76bad47bdcdf2f5321d262268f3f88e874c187da
Slavik <amazingSevenfix@gmail.com>
2024-12-05 04:44:03 +0500

    feat: preparing, proxyController, api

---
+++ app/Controllers/ProxyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class ProxyController extends BaseController
+{
+    public function fetchRates()
+    {
+        //
+    }
+}
