c204c1faf34519fc83b757ba126bcf9fecc0a7e0
Slavik <amazingSevenfix@gmail.com>
2024-12-05 05:16:54 +0500

    feat: curl, fetchRates

--- app/Controllers/ProxyController.php
+++ app/Controllers/ProxyController.php
@@ -17,9 +17,21 @@ class ProxyController extends BaseController
         curl_setopt($ch, CURLOPT_URL, $remoteUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $data = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
         // data collection verification
+        if ($httpCode === 200 && $data) {
+        	return $this->response
+        	  ->setHeader('Content-type', 'application/xml')
+        	  ->setHeader('Access-Control-Allow-Origin', '*')
+        	  ->setBody($data);
+        }
 
+        // if error 
+        return $this->response
+          ->setStatusCode(ResponseInterface::HTTP_BAD_GATEWAY)
+          ->setBody('Не удалось получить данные с удаленного ресурса');
     }
 }
