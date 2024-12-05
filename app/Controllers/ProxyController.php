f1d83af0fab64fc23bcd48d8a66e3a968ed483ef
Slavik <amazingSevenfix@gmail.com>
2024-12-05 05:02:34 +0500

    feat: curl_setopt config

--- app/Controllers/ProxyController.php
+++ app/Controllers/ProxyController.php
@@ -9,6 +9,17 @@ class ProxyController extends BaseController
 {
     public function fetchRates()
     {
-        //
+    	// remote resource
+        $remoteUrl = 'https://nationalbank.kz/rss/rates_all.xml';
+
+        // Initialization CURL
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $remoteUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_close($ch);
+
+        // data collection verification
+
     }
 }
