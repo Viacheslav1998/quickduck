f5329bab1859138d6a36870d4c9e4f22611dfc33
Slavik <amazingSevenfix@gmail.com>
2024-12-21 17:21:16 +0500

    feat: preparing form person

--- public/vue-app/src/views/FormView.vue
+++ public/vue-app/src/views/FormView.vue
@@ -4,18 +4,28 @@ import { defineComponent , ref } from "vue";
 export default defineComponent({
   name: "FormView",
   setup() {
+    // checkbox
     const special = ref(false);
+    
+    // person-form
     const name = ref('');
-    // form elems must have а не по отдельности
-    // const form = new FormData();
+    const email = ref('');
+    const password = ref('');
+    const imagen = ref('');
 
+    // test get data
     const f1 = () => {
       console.log(special.value);
       console.log(name.value);
     }
 
+    const createPerson = async () => {
+      
+    }
+
     return {
       special,
+      createPerson,
       f1,
       name
     };
@@ -29,37 +39,44 @@ export default defineComponent({
       <h2>Регистрация</h2>
     </div>
     <div class="custom-form">
-      <div class="form-group">
-        <label for="name">Выше имя</label>
-        <input v-model="name" type="name" class="form-control" id="name" aria-describedby="name" placeholder="Введите ваше имя">
-        <small id="name" class="form-text text-muted">Имя важно - куда же без него</small>
-      </div>
-      <div class="form-group">
-        <label for="password">Пароль</label>
-        <input v-model="password" type="password" class="form-control" id="password" placeholder="Пароль - придумай, второй раз спрашивать не стану">
-        <small id="password" class="form-text text-muted">Пароль- способ доступа</small>
-      </div>
-      <div class="form-group">
-        <label for="email">Почта</label>
-        <input v-model="email" type="email" class="form-control" id="email" placeholder="Введи пожалуйста твою почту">
-        <small id="email" class="form-text text-muted">Почта поможет, восстановить данные или получать рассылку</small>
-      </div>
-      <div class="form-check">
-        <input 
-          class="form-check-input"
-          type="checkbox"
-          id="toogleFieldCheckbox"
-          v-model="special"
-          aria-label="Отметь если есть код от автора"
-        >
-        <label for="toogleFieldCheckbox"> Есть специальный код ?</label><br>
-      </div>
-      <div class="form-group" v-if="special">
-        <label for="special">спец код</label>
-        <input type="special" class="form-control" id="special" placeholder="специальный код">
-        <small id="special" class="form-text text-muted">Если тебе дали специальный код - пиши его сюда</small>
-      </div>
-      <button type="submit" @click="f1" class="btn btn-primary">Регистрация</button>
+      <form @submit.prevent="createPerson">
+        <div class="form-group">
+          <label for="name">Выше имя</label>
+          <input v-model="name" type="text" class="form-control" id="name" aria-describedby="name" placeholder="Введите ваше имя">
+          <small id="name" class="form-text text-muted">Имя важно - куда же без него</small>
+        </div>
+        <div class="form-group">
+          <label for="name">Твоё изображение</label>
+          <input type="imagen" class="form-control" id="imagen" aria-describedby="imagen">
+          <small id="imagen" class="form-text text-muted">Выбрать изображение - аватарку.</small>
+        </div>
+        <div class="form-group">
+          <label for="password">Пароль</label>
+          <input v-model="password" type="password" class="form-control" id="password" placeholder="Пароль - придумай, второй раз спрашивать не стану">
+          <small id="password" class="form-text text-muted">Пароль- способ доступа</small>
+        </div>
+        <div class="form-group">
+          <label for="email">Почта</label>
+          <input v-model="email" type="email" class="form-control" id="email" placeholder="Введи пожалуйста твою почту">
+          <small id="email" class="form-text text-muted">Почта поможет, восстановить данные или получать рассылку</small>
+        </div>
+        <div class="form-check">
+          <input 
+            class="form-check-input"
+            type="checkbox"
+            id="toogleFieldCheckbox"
+            v-model="special"
+            aria-label="Отметь если есть код от автора"
+          >
+          <label for="toogleFieldCheckbox"> Есть специальный код ?</label><br>
+        </div>
+        <div class="form-group" v-if="special">
+          <label for="special">спец код</label>
+          <input type="text" class="form-control" id="special" placeholder="специальный код">
+          <small id="special" class="form-text text-muted">Если тебе дали специальный код - пиши его сюда</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Регистрация</button>
+      </form>
     </div>
 
     <div class="alert alert-warning" role="alert">
