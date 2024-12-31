e373faa28fcc45b9f1f6f7811bcb5b68e3ba4d9b
Slavik <amazingSevenfix@gmail.com>
2025-01-01 03:33:58 +0500

    feat: css, form

--- public/vue-app/src/views/LoginView.vue
+++ public/vue-app/src/views/LoginView.vue
@@ -19,17 +19,24 @@ export default defineComponent({
     </div>
     <div class="login-box d-flex justify-content-center py-5 mb-2">
       <div class="custom-login">
-        <p>тестовый блок границ</p>
+        <div class="custom-text-login d-flex justify-content-center pt-3">
+          <div>
+            <p> <span style="color: darkorange; font-weight: bold;">Ну</span> ты заходи - если что</p>
+          </div>
+          <div class="custom-icon">
+            <img src="/icons/wolf.png">
+          </div>
+        </div>
         <form action="#" class="p-4">
           <div class="form-group">
-            <label for="email">Введи свою Почту</label>
+            <label for="email" class="custom-input">Введи свою Почту</label>
             <input type="email" class="form-control" id="email" aria-describedby="email" placeholder="Помнишь своё имя ?">
           </div>
           <div class="form-group">
-            <label for="name">Вводи свой Пароль</label>
+            <label for="name" class="custom-input">Вводи свой Пароль</label>
             <input type="password" class="form-control" id="password" aria-describedby="password" placeholder="Пароль это пол дела.">
           </div>
-          <button type="submit" class="btn btn-primary">Входи!</button>
+          <button type="submit" class="btn btn-warning">Входи!</button>
         </form>
       </div>
     </div>
@@ -43,11 +50,35 @@ export default defineComponent({
   background-image: url('../images/login-fone.jpg');
   background-size: contain;
 }
-
-.custom-login {
-  width: 50%;
-  background-color: brown;
+.custom-input {
+  font-family: Arial;
+  font-size: 18px;
+  color: orange;
 }
+.custom-text-login {
+  border-top: 2px solid deepskyblue;
+  background-color: rgb(249, 249, 249);
+}
+.custom-text-login p {
+  color: dimgrey;
+  font-weight: 1000;
+  font-size: larger;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+.custom-login {
+  border-radius: 10px;
+  width: 50%;
+  backdrop-filter: blur(2px);
+  background-color: rgba(55, 70, 152, 0.484);
+}
+.custom-icon img {
+  width: 24px;
+  height: 24px;
+  margin-left: 10px;
+  background-color: deepskyblue;
+  border-radius: 50px;
+}
+
 .begin {
   padding: 20px;
   border-right: 15px solid deeppink;
