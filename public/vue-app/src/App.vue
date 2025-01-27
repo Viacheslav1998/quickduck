67bd9cf8e626f198d8881c813c28e7f82b7662de
Slavik <amazingSevenfix@gmail.com>
2025-01-27 21:39:42 +0500

    feat: methods array, learn

--- public/vue-app/src/App.vue
+++ public/vue-app/src/App.vue
@@ -6,8 +6,9 @@
 
 <script>
 import { defineComponent } from "vue";
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+import '../src/scripts/work_with_array.js';
 
 export default defineComponent({
   components: {
--- public/vue-app/src/scripts/work_with_array.js
+++ public/vue-app/src/scripts/work_with_array.js
@@ -1,27 +1,30 @@
-// just test iteration
-const numbers = [1,2,3,4,5];
-const collectionNumbers = [];
-const lengthNum = numbers.length;
+// Array methods: working with elements
+// ===================================
 
-// even number sorting
-for(let i = 0; i < lengthNum; i++) {
-  if(numbers[i] % 2 === 0) {
-    collectionNumbers.push(numbers[i] * 2);
-  }
-}
+// map - каждый элемент массива возвращает новый массив 
+const arr = [1,2,3,4,5];
+const doubled = arr.map(num => num * 2);
+// console.log(doubled);
 
+// convert to numbers - но основной массив останется целым
+const strNum = ['1','2','3'];
+const numbers = strNum.map(Number);
+//console.log(typeof numbers[0]); // new arr type: Number 
+//console.log(typeof strNum[0]); // arr default = string
 
-// another axample work with pop/push
-const space = [];
-const spb = { name: 'SanktPeterBug' };
-const arrbox = ['name', 'age', 'city']; 
+// ==================================
+// filters
+const arrBoxNum1 = [1,2,3,4,5];
+const evens = arr.filter(num => num % 2 === 0);
+// console.log(evens); // [2,4]
 
-space.push(arrbox);
-space.push(spb);
+// ==================================
+// forEach выполняется для каждого элемента массива - возвращает undefined
+const arrBoxNum2 = [1,2,3,4,5,6,7];
+// arrBoxNum2.forEach(num => console.log(num * 2));
 
-console.log(space);
-space.pop(arrbox);
-console.log(space);
-
-//  console.log(collectionNumbers);
-//  console.log(numbers);
+// ==================================
+// find = возвращает 1 эл. по условию если ничего не найдено return undefined
+const arrBoxNum3 = [66,77,213,523];
+const found = arrBoxNum3.find(num => num > 78);
+// console.log(found) // 213;
