baf48a05af6bdddfe4d4a5352fd78fbf59e5b94a
Slavik <amazingSevenfix@gmail.com>
2025-06-14 21:23:03 +0500

    feat: validate message

--- app/Models/PersonModel.php
+++ app/Models/PersonModel.php
@@ -34,7 +34,30 @@ class PersonModel extends Model
         'password'     => 'required|max_length[255]|min_length[8]',
         'secret'     => 'max_length[30]|alpha_numeric_space|min_length[3]',
     ];
-    protected $validationMessages   = [];
+    protected $validationMessages = [
+        'name' => [
+            'required' => 'Имя обязательно для заполнения.',
+            'max_length' => 'Имя не должно превышать 30 символов.',
+            'min_length' => 'Имя должно содержать не менее 3 символов.',
+            'alpha_numeric_space' => 'Имя может содержать только буквы, цифры и пробелы.',
+        ],
+        'email' => [
+            'required' => 'Электронная почта обязательна.',
+            'max_length' => 'Электронная почта слишком длинная.',
+            'valid_email' => 'Введите корректный email.',
+            'is_unique' => 'Пользователь с такой почтой уже существует.',
+        ],
+        'password' => [
+            'required' => 'Пароль обязателен.',
+            'max_length' => 'Пароль слишком длинный.',
+            'min_length' => 'Пароль должен содержать не менее 8 символов.',
+        ],
+        'secret' => [
+            'max_length' => 'Код слишком длинный.',
+            'min_length' => 'Код должен содержать не менее 3 символов.',
+            'alpha_numeric_space' => 'Код может содержать только буквы, цифры и пробелы.',
+        ],
+    ];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 
