934bd9c1c2bd234b91c0d177a99d2641831ab1a2
Slavik <amazingSevenfix@gmail.com>
2025-07-07 16:56:44 +0500

    feat: comments, quickfix ...

--- public/vue-app/src/components/ui/Card.vue
+++ public/vue-app/src/components/ui/Card.vue
@@ -3,6 +3,9 @@ import { defineComponent, onMounted } from 'vue';
 
 export default defineComponent({
   name: 'Card',
+  props: {
+    title: String
+  },
 
   setup() {
     onMounted(() => {
@@ -15,26 +18,25 @@ export default defineComponent({
 
 <template>
   <div>
-     <div class="bg-dark w-100">
-        <div class="card text-dark" style="width: 18rem;">
-          <slot name="imagen">
-            <img src="..." class="card-img-top" alt="imagen not found">
+    <div class="bg-dark w-100">
+      <div class="card text-dark" style="width: 18rem;">
+        <slot name="imagen">
+          <img src="..." class="card-img-top" alt="imagen not found">
+        </slot>
+        <div class="card-body">
+          <slot name="title">
+            <h5 class="card-title">Card title</h5>
           </slot>
-          <div class="card-body">
-            <slot name="title">
-              <h5 class="card-title">Card title</h5>
-            </slot>
-            <slot name="text"> 
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </slot>
-           <slot name="actions">
-             <a href="#" class="btn btn-primary">next</a>
-           </slot>       
-          </div>
+          <slot name="text"> 
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </slot>
+          <slot name="actions">
+            <a href="#" class="btn btn-primary">next</a>
+          </slot>       
         </div>
+      </div>
     </div>
   </div>
- 
 </template>
-<style>
+<style scoped>
 </style>
