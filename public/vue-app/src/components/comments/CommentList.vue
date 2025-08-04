a6b34ca1415b27537eab100239aea1345218ab4a
Slavik <amazingSevenfix@gmail.com>
2025-08-04 17:45:03 +0500

    feat: user block comment

--- public/vue-app/src/components/comments/CommentList.vue
+++ public/vue-app/src/components/comments/CommentList.vue
@@ -1,25 +1,32 @@
 <script>
-import { ref, onMounted, defineComponent } from 'vue';
+import { ref, onMounted, defineComponent } from 'vue'
+import { useAuthStore } from '@/stores/authStore'
 
 export default defineComponent({
   name: 'CommentList',
   setup() {
+    const auth = useAuthStore()  
     
+    return {
+      auth
+    }
   }
-
 })
 </script>
 
 <template>
   <div>
-     <div class="comment-moment pb-1 pt-2 px-2 mb-4">
-        <h5>Блок для комментариев:</h5>
-      </div>
+    <div class="comment-moment pb-1 pt-2 px-2 mb-4">
+      <h5>Блок для комментариев:</h5>
+    </div>
 
-      <div class="wrapper-comments-persons">
-
+    <div class="wrapper-comments-persons">
+      <!-- проверка если есть комментарий -->
+      <div
+        v-show="auth.isUser"
+      >
         <div>
-          <p>Показаны последние 10 комментариев:</p>
+          <p>Твой комментарий</p>
         </div>
 
         <div class="media">
@@ -33,27 +40,44 @@ export default defineComponent({
             unde harum obcaecati repellendus perferendis aliquid!
           </div>
         </div>
+      </div>
 
-        <div class="media">
-          <img class="mr-3" src="/icons/avatar.png" alt="user" />
-          <div class="media-body">
-            <i>дата комментария: 2021-01-02</i>
-            <hr class="new1" />
-            <h5 class="mt-0"><span>тема:</span> какая то новость</h5>
-            я считаю, что это отличная статья
-          </div>
-        </div>
+      <div>
+        <p>Показаны последние 10 комментариев:</p>
+      </div>
 
-        <div class="media">
-          <img class="mr-3" src="/icons/user.png" alt="user" />
-          <div class="media-body">
-            <i>дата комментария: 2021-01-02</i>
-            <hr class="new1" />
-            <h5 class="mt-0"><span>тема:</span> какая то новость</h5>
-            я считаю, что это отличная статья
-          </div>
+      <div class="media">
+        <img class="mr-3" src="/icons/user.png" alt="user" />
+        <div class="media-body">
+          <i>дата комментария: 2021-01-02</i>
+          <hr class="new1" />
+          <h5 class="mt-0"><span>тема:</span> какая то новость</h5>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis quaerat explicabo esse.
+          Labore fuga eligendi incidunt ea officia sapiente, deserunt aliquam ad laborum soluta
+          unde harum obcaecati repellendus perferendis aliquid!
         </div>
       </div>
+
+      <div class="media">
+        <img class="mr-3" src="/icons/avatar.png" alt="user" />
+        <div class="media-body">
+          <i>дата комментария: 2021-01-02</i>
+          <hr class="new1" />
+          <h5 class="mt-0"><span>тема:</span> какая то новость</h5>
+          я считаю, что это отличная статья
+        </div>
+      </div>
+
+      <div class="media">
+        <img class="mr-3" src="/icons/user.png" alt="user" />
+        <div class="media-body">
+          <i>дата комментария: 2021-01-02</i>
+          <hr class="new1" />
+          <h5 class="mt-0"><span>тема:</span> какая то новость</h5>
+          я считаю, что это отличная статья
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
