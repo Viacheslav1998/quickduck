8317f5230f29cce1b8db566d9dedb3bb8b138fbc
Slavik <amazingSevenfix@gmail.com>
2025-09-06 18:52:19 +0500

    feat:

--- app/Controllers/CommentsController.php
+++ app/Controllers/CommentsController.php
@@ -16,19 +16,48 @@ class CommentsController extends BaseController
 	    $this->model = new CommentsModel();
 	}
 
+
+	/**
+	* get all comments
+	*/
     public function index()
     {
     	$data = $this->model->findAll();
-    	echo "<pre>";
     	print_r($data);
-    	echo "</pre>";
-    	log_message('debug', 'getting data:'. print_r($data, true));
+    	// log_message('debug', 'getting data:'. print_r($data, true));
     }
 
+    /**
+    * add a comment if one does not exits
+    * needed to refactor!!!
+    */
     public function store()
     {
+    	// get data
+    	$current_user_id = $this->request->getJSON(true)['user_id'] ?? null;
+    	$post_id = $this->request->getJSON(true)['post_id'] ?? null;
+
+    	// data availability check
+    	if (!$current_user_id || !$post_id) {
+    		return $this->response->setJSON([
+    			'error' => 'не указан user_id или post_id'
+    		]);
+    	}
+
+    	//  We are executing a request to check for the presence of a comment.
+    	$comment_count = $this->model
+    	                      ->where('user_id', $current_user_id)
+    	                      ->where('post_id', $post_id)
+    	                      ->countAllResults();
+
+    	// Did the user leave a comment?       
+        if ($comment_count > 0) {
+        	return $this->response->setJSON([
+        		'message' => 'Вы уже оставили свой комментарий к данному посту!'
+        	]);
+        }  
+
     	$data = $this->request->getJSON(true);
-    	// log_message('debug', 'received comment data:' . print_r($data, true));
     
     	$comments = $this->model->insert($data);
 
